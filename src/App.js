0ee62be819c449312e805450ab2f4d9275cde1f6
Will Sokolowski <will.sokolowski@gmail.com>
2020-07-02 18:34:08 -0500

    first commit go motus

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import dummyData from "./data";
+import CardList from "./components/CardList";
 
-function App() {
+const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVideos(dummyData);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="App">      
+          {
+            videos.map((list, index) => {
+              return (
+                <section key={index}>
+                  <h2 className="section-title">{list.section}</h2>
+                  <CardList list={list} />
+                  <hr />
+                </section>
+              );
+            })}
+        </div>
+      </>
+    
   );
-}
+};
 
 export default App;
