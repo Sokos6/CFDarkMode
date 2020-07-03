1ad7c2a8cd1d4bb9b0076ad5cfd88b4135777d40
Will Sokolowski <will.sokolowski@gmail.com>
2020-07-02 19:01:43 -0500

    theme provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from './components/Globalstyle';
+import { GlobalStyles } from './components/globalStyles';
 import { lightTheme, darkTheme } from './components/Themes';
 import './App.css';
 import dummyData from './data';
@@ -10,6 +10,7 @@ const App = () => {
   const [videos, setVideos] = useState([]);
   const [theme, setTheme] = useState('light');
 
+  // TODO create theme toggler component
   const themeToggler = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light');
   };
@@ -22,19 +23,23 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <div className='App'>
-        {videos.map((list, index) => {
-          return (
-            <section key={index}>
-              <h2 className='section-title'>{list.section}</h2>
-              <CardList list={list} />
-              <hr />
-            </section>
-          );
-        })}
-      </div>
-    </>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <>
+      <GlobalStyles />
+        <div className='App'>
+          <button onClick={themeToggler}>Switch Theme</button>
+          {videos.map((list, index) => {
+            return (
+              <section key={index}>
+                <h2 className='section-title'>{list.section}</h2>
+                <CardList list={list} />
+                <hr />
+              </section>
+            );
+          })}
+        </div>
+      </>
+    </ThemeProvider>
   );
 };
 
